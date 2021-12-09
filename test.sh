adaca943c03f2fbc23e3e65a3fa3d7850b136059
raghavmnt <95434783+raghavmnt@users.noreply.github.com>
2021-12-09 21:41:05 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,12 @@
 export scripts=/root/scripts
 echo -e "please provide the filename you want to create: \c"
 read -r file
-touch $scripts/$file.sh
-echo '#!/bin/bash" > $scripts/$file.sh
+touch /scripts/$file.sh
+echo '#!/bin/bash" > /scripts/$file.sh
+echo '#Purpose: ' >> /scripts/$file.sh
+echo '#Version: ' >> /scripts/$file.sh
+echo '#Created Date: ' `date` >> /scripts/$file.sh
+echo '#Modified Date: ' >> /scripts/$file.sh
+echo '#Author: Raghav M' >> /scripts/$file.sh
+echo '# START # ' >> /scripts/$file.sh
+echo '# END # ' >> /scripts/$file.sh
